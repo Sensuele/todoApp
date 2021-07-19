eebdf203cfebe8395d13a69f48caa6598db02dad
Евгений Дружняев <druzhniaev@gmail.com>
2021-07-19 22:04:31 +0600

    Add input

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <li className="todo">
+      <div className="todo-item">heyy</div>
+      <button className="complete-btn">BTN</button>
+      <button className="trash-btn">X</button>
+    </li>
+  )
+}
+
+export default Todo
