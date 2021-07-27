fea849471a0c3b5852a6451c7f7c43e49a470094
Евгений Дружняев <druzhniaev@gmail.com>
2021-07-27 16:05:31 +0600

    Filter list items

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-const Form = ({setInputText, todos, setTodos, inputText}) => {
+const Form = ({setInputText, todos, setTodos, inputText, setStatus}) => {
 
   const inputTextHandler = e => {
     setInputText(e.target.value);
@@ -7,18 +7,25 @@ const Form = ({setInputText, todos, setTodos, inputText}) => {
 
   const submitTodoHandler = e => {
     e.preventDefault();
-    setTodos([...todos, {text: inputText, completed: false, id: Math.random() }]);
-    setInputText("");
+
+    if (inputText?.trim() !== "") {
+      setTodos([...todos, {text: inputText, completed: false, id: Math.random() }]);
+      setInputText("");
+    }
+  }
+
+  const statusHandler = e => {
+    setStatus(e.target.value)
   }
   return (
     <div>
       <form>
       <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
-        <i className="fas fa-plus-square">+</i>
+        <i>+</i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
