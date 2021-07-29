90d403c63d6bee920970455f6217b3b170266338
Евгений Дружняев <druzhniaev@gmail.com>
2021-07-29 21:55:57 +0600

    Fix bug local storage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,10 @@ import React from "react";
 import Todo from "./Todo";
 
 const TodoList = ({ todos, setTodos, filteredTodos }) => {
-  console.log(filteredTodos)
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {filteredTodos.map((todo) => (
+        {filteredTodos !== null ? filteredTodos.map((todo) => (
           <Todo
             setTodos={setTodos}
             todos={todos}
@@ -14,7 +13,7 @@ const TodoList = ({ todos, setTodos, filteredTodos }) => {
             key={todo.id}
             todo={todo}
           />
-        ))}
+        )) : ''}
       </ul>
     </div>
   );
